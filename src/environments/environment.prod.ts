e33b19abd157a8ff0a87d2a0ebcf160de557b9fd
mibess <mibexzooo@gmail.com>
2023-12-11 19:16:06 -0300

    feature (#4) remove environments files

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  API_BASE_URL: 'https://cards-api.mibess.com.br',
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  API_BASE_URL: 'http://192.168.15.18:8080',
-};
